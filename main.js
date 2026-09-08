5a1d7b45ec27c7546d1f3acc1c5f6e63077bca75
QuinRob2504 <roberto.quintana2504@gmail.com>
2026-09-07 21:01:37 -0600

    Modifica ubicación de clase

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 /* SIMULADOR: VALIDADOR DE STOCK */
 
-// Función principal del simulador
-function ejecutarSimulador() {
-  // Declaración de clase
-  class Producto {
-    constructor(id, nombre, stock, precio, descripcion) {
-      this.id = id;
-      this.nombre = nombre;
-      this.stock = stock;
-      this.precio = precio;
-      this.descripcion = descripcion;
-    }
-    verificarStock() {
-      if (this.stock > 0) {
-        return `El producto "${this.nombre}" está disponible. Stock: ${this.stock}`;
-      } else {
-        return `El producto "${this.nombre}" está agotado.`;
-      }
-    }
+// Declaración de clase
+class Producto {
+  constructor(id, nombre, stock, precio, descripcion) {
+    this.id = id;
+    this.nombre = nombre;
+    this.stock = stock;
+    this.precio = precio;
+    this.descripcion = descripcion;
   }
 
+  verificarStock() {
+    if (this.stock > 0) {
+      return `El producto "${this.nombre}" está disponible. Stock: ${this.stock}`;
+    } else {
+      return `El producto "${this.nombre}" está agotado.`;
+    }
+  }
+}
+
+// Función principal del simulador
+function ejecutarSimulador() {
   // Declaración de variables
   const precioUnitario = 5;
   const stock = 10;
